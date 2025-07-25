7729ca2139ecdaf895a4b2ef3235fd09f1fd6aa6
Onur Aktas <onuraktas0640@gmail.com>
2025-07-25 23:00:43 +0300

    TestData

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -9,5 +9,40 @@ public class DataContext : DbContext
     {
     }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Product>().HasData(
+            new List<Product>
+            {
+                new Product
+                {
+                    Id = 1, Name = "Iphone 16 Pro", Description = "256 GB", Price = 55000, IsActive = true, Stock = 20,
+                    ImageUrl = "1.jpg"
+                },
+                new Product
+                {
+                    Id = 2, Name = "Iphone 15 Pro", Description = "512 GB", Price = 45000, IsActive = true, Stock = 13,
+                    ImageUrl = "2.jpg"
+                },
+                new Product
+                {
+                    Id = 3, Name = "Macbook Air", Description = "256 GB", Price = 39000, IsActive = false, Stock = 50,
+                    ImageUrl = "3.jpg"
+                },
+                new Product
+                {
+                    Id = 4, Name = "Iphone 16 Pro Max", Description = "512 GB", Price = 85000, IsActive = true, Stock = 40,
+                    ImageUrl = "4.jpg"
+                },
+                new Product
+                {
+                    Id = 5, Name = "Samsung S24 Plus", Description = "256 GB", Price = 53000, IsActive = true, Stock = 90,
+                    ImageUrl = "5.jpg"
+                }
+            }
+        );
+    }
+
     public DbSet<Product> Products { get; set; }
 }
