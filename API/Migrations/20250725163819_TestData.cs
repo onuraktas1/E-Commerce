be6bffc6c2add7c20dff0a38264e107d622078d7
Onur Aktas <onuraktas0640@gmail.com>
2025-07-25 23:00:43 +0300

    TestData

---
+++ API/Migrations/20250725163819_TestData.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class TestData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "IsActive", "Name", "Price", "Stock" },
+                values: new object[,]
+                {
+                    { 1, "256 GB", "1.jpg", true, "Iphone 16 Pro", 55000m, 20 },
+                    { 2, "512 GB", "2.jpg", true, "Iphone 15 Pro", 45000m, 13 },
+                    { 3, "256 GB", "3.jpg", false, "Macbook Air", 39000m, 50 },
+                    { 4, "512 GB", "4.jpg", true, "Iphone 16 Pro Max", 85000m, 40 },
+                    { 5, "256 GB", "5.jpg", true, "Samsung S24 Plus", 53000m, 90 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 5);
+        }
+    }
+}
