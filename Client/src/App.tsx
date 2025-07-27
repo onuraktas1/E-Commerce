ea0faba5ef9bf78fb114398ada9041ff3502a40d
Onur Aktas <onuraktas0640@gmail.com>
2025-07-27 03:32:24 +0300

    Update App.tsx

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,9 +1,56 @@
+const products = [
+    {
+        id: 1, name: "Product 1", price: 1000, is_active: true
+    },
+    {
+        id: 2, name: "Product 2", price: 200, is_active: false
+    },
+    {
+        id: 3, name: "Product 3", price: 600, is_active: true
+    }
+]
 
 function App() {
-  
-  return (
-    <h1>React.js</h1>
-  )
+    return (
+        <>
+            <Header/>
+            <ProductList/>
+        </>
+    )
+}
+
+function Header() {
+    return (
+        <h1>Header</h1>
+    );
+}
+
+function ProductList() {
+    return (
+        <div>
+            <h2>ProductList</h2>
+            {
+                products.map(p =>
+                    (<Product key={p.id} product={p}/>))
+            }
+        </div>
+    );
+}
+
+function Product(props: any) {
+    return (
+        <>
+            {
+                props.product.is_active ? (
+                    <div>
+                        <h3>{props.product.name}</h3>
+                        <p>{props.product.price}</p>
+                    </div>
+                ) : <p>Ürün satışta değil</p>
+            }
+        </>
+
+    );
 }
 
 export default App
