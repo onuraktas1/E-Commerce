d21f47bad163e9e1031d0195113af1f5fe77f28f
Onur Aktas <onuraktas0640@gmail.com>
2025-07-27 14:13:05 +0300

    Update App.tsx

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,14 +1,4 @@
-const products = [
-    {
-        id: 1, name: "Product 1", price: 1000, is_active: true
-    },
-    {
-        id: 2, name: "Product 2", price: 200, is_active: false
-    },
-    {
-        id: 3, name: "Product 3", price: 600, is_active: true
-    }
-]
+import {useState} from "react";
 
 function App() {
     return (
@@ -26,6 +16,23 @@ function Header() {
 }
 
 function ProductList() {
+    const [products, setProducts] = useState([
+        {
+            id: 1, name: "Product 1", price: 1000, is_active: true
+        },
+        {
+            id: 2, name: "Product 2", price: 200, is_active: false
+        },
+        {
+            id: 3, name: "Product 3", price: 600, is_active: true
+        }])
+
+    function addProduct() {
+        setProducts([...products, {id: 4, name: "Product 4", price: 690, is_active: true}])
+    }
+    
+    console.log("Render...")
+
     return (
         <div>
             <h2>ProductList</h2>
@@ -33,6 +40,8 @@ function ProductList() {
                 products.map(p =>
                     (<Product key={p.id} product={p}/>))
             }
+
+            <button onClick={addProduct}>Add Product</button>
         </div>
     );
 }
