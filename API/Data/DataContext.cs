bb5bf182f71bc2631fb8dfa3c3a8b574d5613d86
Onur Aktas <onuraktas0640@gmail.com>
2025-08-27 01:46:21 +0300

    Update DataContext.cs

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -7,6 +7,7 @@ public class DataContext : DbContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
+        
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
