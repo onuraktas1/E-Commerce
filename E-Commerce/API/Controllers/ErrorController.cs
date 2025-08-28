86efdd66a66284be69de07c1a9899920981fa6ae
Onur Aktas <onuraktas0640@gmail.com>
2025-08-28 14:21:19 +0300

    Create ErrorController.cs

---
+++ E-Commerce/API/Controllers/ErrorController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("/api/[controller]")]
+public class ErrorController : ControllerBase
+{
+    [HttpGet("not-found")]
+    public IActionResult NotFoundError()
+    {
+        return NotFound();
+    }
+
+    [HttpGet("bad-request")]
+    public IActionResult BadRequestError()
+    {
+        return BadRequest();
+    }
+
+    [HttpGet("unauthorized")]
+    public IActionResult UnAuthorizedError()
+    {
+        return Unauthorized();
+    }
+
+    [HttpGet("server-error")]
+    public IActionResult ServerError()
+    {
+        throw new Exception("Server Error");
+    }
+
+    [HttpGet("validation-error")]
+    public IActionResult ValidationError()
+    {
+        ModelState.AddModelError("Validation Error 1", "validation-error details");
+        ModelState.AddModelError("Validation Error 2", "validation-error details");
+        return ValidationProblem();
+    }
+}
