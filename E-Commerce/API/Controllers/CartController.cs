2207b03a0e4757a4dcd029a4296c39148aa47762
Onur Aktas <onuraktas0640@gmail.com>
2025-09-01 19:52:45 +0300

    CartDTO

--- E-Commerce/API/Controllers/CartController.cs
+++ E-Commerce/API/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 ﻿using API.Data;
+using API.DTO;
 using API.Entity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -17,10 +18,9 @@ public class CartController : ControllerBase
     }
 
     [HttpGet]
-    public async Task<ActionResult<Cart>> GetCart()
+    public async Task<ActionResult<CartDTO>> GetCart()
     {
-        var cart = await GetOrCreate();
-        return cart;
+        return CartToDTO(await GetOrCreate());
     }
 
     [HttpPost]
@@ -35,9 +35,21 @@ public class CartController : ControllerBase
         cart.AddItem(product, quantity);
         var result = await _context.SaveChangesAsync() > 0;
         if (result)
-            return CreatedAtAction(nameof(GetCart), cart);
+            return CreatedAtAction(nameof(GetCart), CartToDTO(cart));
 
         return BadRequest(new ProblemDetails { Title = "The product can not be added to cart" });
+    }
+
+    [HttpDelete]
+    public async Task<ActionResult> DeleteItemFromCart(int productId, int quantity)
+    {
+        var cart = await GetOrCreate();
+        cart.DeleteItem(productId, quantity);
+        var result = await _context.SaveChangesAsync() > 0;
+        if (result)
+            return Ok();
+
+        return BadRequest(new ProblemDetails { Title = "Problem removing item from the cart" });
     }
 
     private async Task<Cart> GetOrCreate()
@@ -63,5 +75,22 @@ public class CartController : ControllerBase
         }
 
         return cart;
+    }
+
+    private CartDTO CartToDTO(Cart cart)
+    {
+        return new CartDTO
+        {
+            CartId = cart.CartId,
+            CustomerId = cart.CustomerId,
+            CartItems = cart.CartItems.Select(item => new CartItemDTO
+            {
+                ProductId = item.ProductId,
+                Name = item.Product.Name,
+                Price = item.Product.Price,
+                Quantity = item.Quantity,
+                ImageUrl = item.Product.ImageUrl
+            }).ToList()
+        };
     }
 }
