ab1222eaa5b5afe5c7cbf55c8068308dc17ffb5a
Onur Aktas <onuraktas0640@gmail.com>
2025-10-03 14:12:10 +0300

    redux tool kit

--- E-Commerce/Client/src/main.tsx
+++ E-Commerce/Client/src/main.tsx
@@ -4,11 +4,15 @@ import './index.css'
 import {RouterProvider} from "react-router";
 import {router} from "./Router/Routes.tsx";
 import {CartContextProvider} from "./Context/CartContext.tsx";
+import {Provider} from "react-redux";
+import {store} from "./store/store.ts";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
+        <Provider store={store}>
         <CartContextProvider>
             <RouterProvider router={router}/>
         </CartContextProvider>
+        </Provider>
     </StrictMode>,
 )
---
+++ E-Commerce/Client/src/store/store.ts
@@ -0,0 +1,11 @@
+﻿import {configureStore} from "@reduxjs/toolkit";
+import {counterSlice} from "../Pages/counter/counterSlice.ts";
+
+export const store=configureStore({
+    reducer:{
+        counter: counterSlice.reducer
+    }
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch=typeof  store.dispatch;
